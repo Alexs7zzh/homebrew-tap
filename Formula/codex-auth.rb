4c2cdd2e413297ffadc9f79936ec649ab7148318
Alex Zhou <alex.zzh.s7@gmail.com>
2026-03-09 17:04:29 +0900

    Update codex-auth to 0.2.0

--- Formula/codex-auth.rb
+++ Formula/codex-auth.rb
@@ -1,15 +1,15 @@
 class CodexAuth < Formula
   desc "Interactive local Codex account switcher"
   homepage "https://github.com/Alexs7zzh/codex-auth"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Alexs7zzh/codex-auth/releases/download/v#{version}/codex-auth_v#{version}_darwin_arm64.tar.gz"
-      sha256 "be4e5a6ee6ade03a22f4e37dfdcd8e80f4cd9b8691a602db853dd9613531de80"
+      sha256 "4de14c5ce0881b1ea10ae0cff97a8c57e20be38be4e53cda131a9532b2672e75"
     else
       url "https://github.com/Alexs7zzh/codex-auth/releases/download/v#{version}/codex-auth_v#{version}_darwin_amd64.tar.gz"
-      sha256 "95253a1716cf633d4faaf35149af04211946c4885fcbbb91afeae3036ef97da6"
+      sha256 "552d61712557f5d3e0c683fd7c2cf523504b8742568a47052945cd0a216f2df3"
     end
   end
 
